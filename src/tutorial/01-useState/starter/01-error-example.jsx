7d6ce1331c9883d31aad27a8ce26f8393e18ed45
Filip <fpodsiadly@gmail.com>
2023-05-06 13:10:52 +0200

    setState error and basic count

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,21 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+
+  const handleClick = () => {
+    count++;
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        add
+      </button>
+    </div>
+  );
 };
+
+
 
 export default ErrorExample;
